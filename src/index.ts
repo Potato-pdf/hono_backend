6fe7d4b0f80f4ca825413e8f562321a5a4378a37
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-04 12:11:56 -0600

    feat: index.middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
+import { prettyJSON } from 'hono/pretty-json'
 
 const app = new Hono()
 const port = 8000
 
+//middleware
+app.use(cors())
+app.use(logger())
+app.use(prettyJSON())
+
+//routes
 
 
+
+//serve
 export default {
   port :port,
   fetch: app.fetch
 }
 
-console.log(`Bun it is located in port  ${port}`)

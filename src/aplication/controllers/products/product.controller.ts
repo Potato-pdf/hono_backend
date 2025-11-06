c64d50f3e865d71f615a62be96d02060ff7afc9d
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-06 12:47:07 -0600

    product: interface

--- src/aplication/controllers/products/product.controller.ts
+++ src/aplication/controllers/products/product.controller.ts
@@ -4,10 +4,10 @@ import { product } from "../../../domain/entities/products/products.entitie"
 
 const product_repository = AppDataSource.getRepository(product);
 
-export const create_product = (c : Context)=>{
-    try{
-        
-    return c.json({test:"controller"})
+export const create_product = async (c : Context)=>{
+    try {
+        const prod = await c.req.json()
+        return c.json({test:"controller"})
         
     }
     catch(err){
---
+++ src/domain/interfaces/product.interface.d.ts
@@ -0,0 +1,6 @@
+export interface producto_iinterface{
+    name:string
+    description: string
+    price:number
+    stock:number
+}

169e94a6cadf7178de36ebf084bbf705330ea5a9
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-06 13:30:19 -0600

    feat: user controller v1

--- src/aplication/controllers/users/users.controller.ts
+++ src/aplication/controllers/users/users.controller.ts
@@ -1,7 +1,18 @@
 import { Context } from "hono"
+import { AppDataSource } from "../../../Infrestructure/config/database/db.conection"
+import { product_entitie } from "../../../domain/entities/products/products.entitie"
+
+const user_repository = AppDataSource.getRepository(product_entitie)
 
 export const create_user = (c: Context) => {
+    try{
+        
     return c.json({test:"controler"})
+    }
+    catch(err){
+        console.error(`Error: ${err}`)
+        return c.json({error: "Server error", err}, 400);
+    }
 }
 
 export const get_all_users = (c:Context) => {

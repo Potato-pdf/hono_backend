dac927ec0eed8a1b143e5d4b0dd29450adca3f57
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-06 13:39:59 -0600

    feat: user create  controller v2

--- src/aplication/controllers/users/users.controller.ts
+++ src/aplication/controllers/users/users.controller.ts
@@ -1,12 +1,19 @@
 import { Context } from "hono"
 import { AppDataSource } from "../../../Infrestructure/config/database/db.conection"
-import { product_entitie } from "../../../domain/entities/products/products.entitie"
+import { user_interface } from "../../../domain/interfaces/user/user.interface"
+import { user_entitie } from "../../../domain/entities/user/user.entitie"
 
-const user_repository = AppDataSource.getRepository(product_entitie)
+const user_repository = AppDataSource.getRepository(user_entitie)
 
-export const create_user = (c: Context) => {
+export const create_user = async (c: Context) => {
     try{
-        
+        const usr : user_interface = await c.req.json()
+        const user = await user_repository.create({
+            name : usr.name,
+            email : usr.email,
+            password : usr.password
+        });
+
     return c.json({test:"controler"})
     }
     catch(err){

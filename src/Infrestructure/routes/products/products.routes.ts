4224b398c6b2d9d811aa77ecf77db75aacf25d4a
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-06 20:17:21 -0600

    feat: fin by id

--- src/Infrestructure/routes/products/products.routes.ts
+++ src/Infrestructure/routes/products/products.routes.ts
@@ -6,7 +6,7 @@ export const product = new Hono()
 
 product.post("/create", controller.create_product)
 product.get("/all", controller.get_all_products)
-product.get("/product", controller.get_product_by_id)
+product.get("/product/:id", controller.get_product_by_id)
 product.put("/update", controller.update_product)
-product.delete("/delete", controller.delete_product)
+product.delete("/delete/:id", controller.delete_product);
 
--- src/aplication/controllers/products/product.controller.ts
+++ src/aplication/controllers/products/product.controller.ts
@@ -5,51 +5,88 @@ import { producto_iinterface } from "../../../domain/interfaces/product/product.
 
 const product_repository = AppDataSource.getRepository(product_entitie);
 
-export const create_product = async (c : Context)=>{
+export const create_product = async (c: Context) => {
     try {
-        const prod : producto_iinterface= await c.req.json()
+        const prod: producto_iinterface = await c.req.json()
         const product = product_repository.create({
-            name:prod.name,
-            price:prod.price,
-            description:prod.description,
-            stock:prod.stock
+            name: prod.name,
+            price: prod.price,
+            description: prod.description,
+            stock: prod.stock
         })
         await product_repository.save(product)
         c.status(201)
-        return c.json({test:"succes create", product})
+        return c.json({ test: "succes create", product })
     }
-    catch(err){
+    catch (err) {
         console.error(`error:${err}}`)
-        return c.json({error:"Server error", err}, 500
+        return c.json({ error: "Server error", err }, 500
         );
     }
 }
 
-export const get_all_products = async (c : Context)=>{
-    try{
-        const all_products= await product_repository.find({
-
-        })
+export const get_all_products = async (c: Context) => {
+    try {
+        const all_products = await product_repository.find({})
         c.status(200)
-        return c.json({test:"Get products", data: all_products})
+        return c.json({ test: "Get products", data: all_products })
     }
-    catch(err){
+    catch (err) {
         console.error(`error : ${err}`)
         c.status(500);
         return c.json({ error: "Server error", details: err });
 
-        
+
     }
 }
 
-export const get_product_by_id = (c:Context)=>{
-    return c.json({test:"controller"})
+export const get_product_by_id = async (c: Context) => {
+    try {
+        const id = c.req.param("id");
+        const product = await product_repository.findOneBy({ id: parseInt(id) });
+
+        if (!product) {
+            c.status(404);
+            return c.json({ error: "Product not found" });
+        }
+
+        c.status(200);
+        return c.json({ message: "Product found", data: product });
+    } catch (err) {
+        console.error("Error:", err);
+        c.status(500);
+        return c.json({ error: "Server error", details: err });
+    }
+};
+
+
+
+export const update_product = (c: Context) => {
+    return c.json({ test: "controller" })
 }
 
-export const update_product = (c:Context) => {
-    return c.json({test:"controller"})
-}
 
-export const delete_product = (c: Context) => {
-    return c.json({test:"controller"})
-}
+export const delete_product = async (c: Context) => {
+  console.log("params:", c.req.param("id"));
+  try {
+    const id = c.req.param("id");
+    const product_to_delete = await product_repository.findOneBy({ id: parseInt(id) });
+
+    if (!product_to_delete) {
+      c.status(404);
+      return c.json({ error: "Product not found" });
+    }
+
+    await product_repository.remove(product_to_delete);
+
+    c.status(200);
+    return c.json({
+      message: "Product deleted successfully",
+      deleted: product_to_delete,
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    c.status(500);
+    return c.json({ error: "Server error", details: err });
+  }
+};

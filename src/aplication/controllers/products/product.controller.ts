d70fbed31db5a9cad1899030fcff5c7cb7e554ad
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-06 19:38:54 -0600

    fix: delete version

--- src/aplication/controllers/products/product.controller.ts
+++ src/aplication/controllers/products/product.controller.ts
@@ -8,24 +8,38 @@ const product_repository = AppDataSource.getRepository(product_entitie);
 export const create_product = async (c : Context)=>{
     try {
         const prod : producto_iinterface= await c.req.json()
-        const product = await product_repository.create({
+        const product = product_repository.create({
             name:prod.name,
             price:prod.price,
             description:prod.description,
             stock:prod.stock
         })
+        await product_repository.save(product)
         c.status(201)
         return c.json({test:"succes create", product})
     }
     catch(err){
         console.error(`error:${err}}`)
-        return c.json({error:"Server error", err}, 400
+        return c.json({error:"Server error", err}, 500
         );
     }
 }
 
-export const get_all_products = (c : Context)=>{
-    return c.json({test:"controller"})
+export const get_all_products = async (c : Context)=>{
+    try{
+        const all_products= await product_repository.find({
+
+        })
+        c.status(200)
+        return c.json({test:"Get products", data: all_products})
+    }
+    catch(err){
+        console.error(`error : ${err}`)
+        c.status(500);
+        return c.json({ error: "Server error", details: err });
+
+        
+    }
 }
 
 export const get_product_by_id = (c:Context)=>{

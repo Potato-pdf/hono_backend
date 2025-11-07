658177f18a4b4cc3483bdeefeadcc86aee48d2e2
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-07 11:54:26 -0600

    feat: mejor uppercammelcase

--- src/domain/utils/upper.case.utils.ts
+++ src/domain/utils/upper.case.utils.ts
@@ -1,6 +1,6 @@
 
-export const toCamelCase = (name: string): string => {
+export const toPascalCase = (name: string): string => {
     const words = name.toLowerCase().split(/\s+/);
     if (words.length === 0) return '';
-    return words[0] + words.slice(1).map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
+    return words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
 }
